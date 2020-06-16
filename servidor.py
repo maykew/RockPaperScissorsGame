713434af718f491475906c6a04ba0df90ed83246
Mayke Willians <maykewillians3@gmail.com>
2020-06-16 11:14:19 -0300

    Division of servidor and cliente code into functions

--- servidor.py
+++ servidor.py
@@ -2,6 +2,49 @@ import socket
 HOST = ''              # Endereco IP do Servidor e o endereco atual do computador
 PORT = 5000            # Porta do Servidor na maquina 
 
+#-------------- Funcoes --------------
+
+def conexao():
+    for i in range(numJogadores):
+        con, cliente = tcp.accept() # Aceita conexao do cliente
+        clientes.append((con, cliente)) #Adicionado cliente na lista de clientes
+        print ('Concetado por', clientes[i][1])
+
+def enviaMensagem(msg, destino):
+    msg = msg.encode('UTF-8')       # Codifica a mensagem para UTF-8
+    destino.send(msg)     #Envia mensagem ao cliente
+
+def recebeMensagem(remetente):
+    msg = remetente.recv(1024)		# Le uma mensgem vinda do cliente
+    msg = msg.decode('UTF-8')	# Decodifica a mensagem
+    return msg
+
+def identificacao():
+    for i in range(numJogadores):
+        #Enviando mensagem inicial
+        msg_inicial = "\nHi! Welcome to Rock, Paper, Scissors Game\n"
+        enviaMensagem(msg_inicial, clientes[i][0])
+
+        #Enviando mensagem para identificação do jogador
+        msg_nome = "Type your name: "
+        enviaMensagem(msg_nome, clientes[i][0])
+
+        #Recebendo mensagem para identificação do jogador
+        msg_nome = recebeMensagem(clientes[i][0])
+        jogadores.append(msg_nome) #Adiciona nome do jogador a lista de jogadores
+    
+def adversarios():
+    #Enviando mensagem aos jogadores da rodada
+    for i in range(numJogadores):
+        adversarios=[]
+        for j in range(1,numJogadores):
+            adversarios.append(jogadores[(i+j)%numJogadores])
+        msg="\n"+jogadores[i]+", your opponents are: "+", ".join(adversarios[:len(adversarios)-1])+" and " + adversarios[-1]
+        enviaMensagem(msg, clientes[i][0])
+
+
+
+
 #-------------- Cria o socket do servidor --------------
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT) # Forma a tupla de host e porta
@@ -13,55 +56,42 @@ tcp.listen(10)		# Entra no modo de escuta
 
 while True:
 
+    #Numero de jogadores
+    numJogadores = 3
+
     #Lista de clientes
     clientes = []
     
     #lista de nomes dos jogadores
     jogadores=[]
 
-    for i in range(3):
-        con, cliente = tcp.accept() # Aceita conexao do cliente
-        clientes.append((con, cliente)) #Adicionado cliente na lista de clientes
-        print ('Concetado por', clientes[i][1])
+    #Realiza a conexao dos clientes
+    conexao()
 
-        #Enviando mensagem inicial
-        msg_inicial = "\nHi! Welcome to Rock, Paper, Scissors Game\n"
-        msg_inicial = msg_inicial.encode('UTF-8')       # Codifica a mensagem para UTF-8
-        clientes[i][0].send (msg_inicial)     #Envia mensagem inicial ao cliente
-
-        #Enviando mensagem para identificação do jogador
-        msg_nome = "Type your name: "
-        msg_nome = msg_nome.encode('UTF-8')       # Codifica a mensagem para UTF-8
-        clientes[i][0].send (msg_nome)     #Envia mensagem para identificação do jogador
-
-        #Recebendo mensagem para identificação do jogador
-        msg_nome = clientes[i][0].recv(1024)		# Le uma mensgem vinda do cliente
-        msg_nome = msg_nome.decode('UTF-8')	# Decodifica a mensagem
-        jogadores.append(msg_nome) #Adiciona nome do jogador a lista de jogadores
-
-    #Enviando mensagem aos jogadores da rodada
-    for i in range(3):
-        msg="\n"+jogadores[i]+", your opponents are "+jogadores[(i+1)%3]+" and "+jogadores[(i+2)%3]
-        msg = msg.encode('UTF-8')       # Codifica a mensagem para UTF-8
-        clientes[i][0].send (msg)     #Envia mensagem ao cliente
+    #Realiza a identificacao dos clientes
+    identificacao()
+    
+    #Envia mensagem aos jogadores da rodada
+    adversarios()
 
     #------- Troca de mensagens --------------
     while True:
+
         #Recebendo mensagem de jogadas
         mensagens = []  #Lista de mensagens 
-        for i in range(3):
-            msg = clientes[i][0].recv(1024)		# Recebe mensagem do cliente         
+        for i in range(numJogadores):
+            msg = recebeMensagem(clientes[i][0])      
             if not msg: break
             mensagens.append(msg)     # Armazena mensagem          
         if not msg: break
 
-        for i in range(3):
-            for j in range(1,3):
-                clientes[(i+j)%3][0].send (mensagens[i])	    		# Envia a mensagem para o cliente 
+        for i in range(numJogadores):
+            for j in range(1,numJogadores): 
+                enviaMensagem(mensagens[i], clientes[(i+j)%numJogadores][0])   		# Envia a mensagem para o cliente 
 
     #---------------- fim do protocolo --------------
 
-    for i in range(3):
+    for i in range(numJogadores):
         print ('Finalizando conexao do cliente', clientes[i][1])
         clientes[i][0].close()		# fecha a conexao com o cliente
 
